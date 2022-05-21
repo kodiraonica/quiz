cb26b2a1c17d2cdb69e307a664e2e10cdc26ba4b
Christian Krajna <christian.krajna@gmail.com>
2022-05-21 10:43:39 +0200

    appendanje buttona

--- main.js
+++ main.js
@@ -21,4 +21,17 @@ questionsData.forEach((q) => {
     const h2 = document.createElement("h2");
     h2.innerHTML = q.questions;
     div.append(h2);
+
+   // console.log(q.options);
+
+   q.options.forEach((option) =>{
+
+    const button = document.createElement("button");
+    button.setAttribute("class","option");
+    button.innerHTML = option;
+    div.append(button);
+    
+
+   })
+
 })
