bf7f7c7743f5ba0f73cb806ff719078ed70b8721
zmaaj <48415383+zmaaj@users.noreply.github.com>
2022-05-21 10:30:52 +0200

    show all questions

---
+++ main.js
@@ -0,0 +1,21 @@
+const questionsData = [
+		{
+			question: "Kako se zoves?"
+			options: ["Stef","Jura","Leon"]
+			answer: "Leon"
+		},
+		{
+			question:"Kako se prezivas?"
+			options:["Repalust", "Novak", "Horvat"]
+			answer: "Repalust"
+		}
+]
+
+questionsData.forEach((q)=>{
+	const div = document.createElement("div");
+	div.setAttribute("class", "question");
+	document.body.append(div);
+	const h2 = document.createElement("h2")
+	h2.innerHTML = q.question;
+	div.append(h2);
+})
