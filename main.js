dd4ccdd20190cc4816a08f4a7046e9502e319cc1
Viktor Zbodulja <viktorzbodulja@gmail.com>
2022-05-21 10:35:30 +0200

    sva pitanja

---
+++ main.js
@@ -0,0 +1,21 @@
+const questionsData = [
+    {
+        question: "Kako se zoveš?",
+        options: ["Štef", "Jura", "Pero"],
+        answer: "Štef" 
+    },
+    {
+        question: "Kako se prezivaš?",
+        options: ["Štefić", "Jurić", "Perić"],
+        answer: "Štefić" 
+    }
+]
+  
+questionsData.forEach((q) => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "question");
+    document.body.append(div);
+
+    const h2 = document.createElement;
+})
+   
