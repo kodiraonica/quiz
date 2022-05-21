22f1fe97b3e8ede1b4a06c2deb4a4a2727ccfea1
Luka Liker <liker.luka@gmail.com>
2022-05-21 11:45:34 +0200

    near end

--- main.js
+++ main.js
@@ -10,7 +10,24 @@ const questionData = [
     options: ["Stefic", "Juric", "Peric"],
     answer: "Stefic",
   },
+  {
+    question: "Kako se prezivas2?",
+    options: ["Stefic", "Juric", "Peric"],
+    answer: "Stefic",
+  },
+  {
+    question: "Kako se prezivas3?",
+    options: ["Stefic", "Juric", "Peric"],
+    answer: "Stefic",
+  },
 ];
+
+let currentQuestion = 0;
+let interval;
+
+const timer = document.createElement("h3");
+timer.innerHTML = 10;
+document.body.append(timer);
 
 questionData.forEach((q) => {
   const div = document.createElement("div");
@@ -33,10 +50,31 @@ questionData.forEach((q) => {
 });
 
 const buttons = document.getElementsByClassName("option");
+const questions = document.getElementsByClassName("question");
+// ovime prikazujemo prvo pitanje
+questions[currentQuestion].style.display = "block";
+
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", () => {
     if (button.getAttribute("data-correct")) {
       alert("correct");
     } else alert("incorrect");
+
+    if (currentQuestion < questions.length - 1) {
+      // ovim pokazujemo iduca pitanja
+      questions[currentQuestion].style.display = "none";
+      currentQuestion++;
+      questions[currentQuestion].style.display = "block";
+      clearInterval(interval);
+      timer.innerHTML = 10;
+      startInterval();
+    }
   });
 });
+
+startInterval();
+function startInterval() {
+  interval = setInterval(() => {
+    if (timer.innerHTML > 0) timer.innerHTML = timer.innerHTML - 1;
+  }, 1000);
+}
