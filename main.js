131d304990fa86db6ecc40667486d0130b373575
Antonija Simic <antonija1023@gmail.com>
2022-05-28 09:46:20 +0200

    add score

--- main.js
+++ main.js
@@ -17,59 +17,93 @@ const questionsData = [
 ]
 let interval;
 let currentQuestion = 0;
+let score = 0;
+let scoreElement;
 const questions = document.getElementsByClassName("question");
-
-questionsData.forEach((q) => {
- const div = document.createElement("div");
- const h2 = document.createElement("h2")
- div.setAttribute("class", "question");
- document.body.append(div);
- h2.innerHTML = q.question;
- div.append(h2);
-
- q.options.forEach((option, index) => {
-    const button = document.createElement("button");
-    button.setAttribute("class", "option");
-    button.innerHTML = option;
-    if (option === q.answer) {
-        button.setAttribute("data-correct", true)
-    }
-    Array.from(questions).forEach((question) => {
-        question.append(button)
-        questions[currentQuestion].style.display = "block";
-    })
- })
-})
-
-const buttons = document.getElementsByClassName("option");
-Array.from(buttons).forEach((button) => {
-    button.addEventListener("click", () => {
-        if (button.getAttribute("data-correct")) {
-           if (currentQuestion < questions.length - 1) {
-            showNewQuestion();
-            alert("correct")
-           } else {
-            clearInterval(interval)
-            timer.innerHTML = 10;
-            console.log("no more questions");
-           }
-        } else {
-            if (currentQuestion < questions.length - 1) {
-               showNewQuestion();
-               alert("incorrect")
-            } else {
-                clearInterval(interval)
-                timer.innerHTML = 10;
-                alert("no more questions")
-            }
-
-        }
-    })
-});
-
 const timer = document.createElement("h3");
-timer.innerHTML = 10;
-document.body.append(timer);
+const buttonStart = document.createElement("button");
+const buttons = document.getElementsByClassName("option");
+
+buttonStart.innerHTML = "Start Quiz";
+buttonStart.setAttribute("class", "btn-start");
+document.body.append(buttonStart);
+buttonStart.addEventListener("click", initGame);
+
+getScoreFromLocalStorage();
+
+function initGame(){
+    createTimer();
+    createResetButton();
+    createScoreElement();
+    updateScore();
+
+    questionsData.forEach((q) => {
+        const div = document.createElement("div");
+        const h2 = document.createElement("h2")
+        div.setAttribute("class", "question");
+        document.body.append(div);
+        h2.innerHTML = q.question;
+        div.append(h2);
+       
+        q.options.forEach((option) => {
+           const button = document.createElement("button");
+           button.setAttribute("class", "option");
+           button.innerHTML = option;
+           if (option === q.answer) {
+               button.setAttribute("data-correct", true)
+           }
+           Array.from(questions).forEach((question) => {
+               question.append(button)
+               questions[currentQuestion].style.display = "block";
+           })
+        })
+       })
+       
+       Array.from(buttons).forEach((button) => {
+           button.addEventListener("click", () => {
+               if (button.getAttribute("data-correct")) {
+                  if (currentQuestion < questions.length - 1) {
+                   showNewQuestion();
+                   score++
+                   updateScore();
+                  } else {
+                    score++
+                    updateScore();
+                    clearInterval(interval);
+                    alert("no more questions");
+                  }
+               } else {
+                   if (currentQuestion < questions.length - 1) {
+                      showNewQuestion();
+                      score--
+                      updateScore();
+                   } else {
+                       score--
+                       updateScore();
+                       clearInterval(interval);
+                       alert("no more questions")
+                   }
+       
+               }
+           })
+       });
+}
+
+function getScoreFromLocalStorage() {
+   const userScore = localStorage.getItem("score");
+   userScore && alert(`Your last score was: ${userScore}`)
+}
+
+function updateScore() {
+    scoreElement.innerHTML = `Your score is ${score}`;
+    localStorage.setItem("score", score);
+}
+
+function createTimer(){
+    timer.innerHTML = 10;
+    document.body.append(timer);    
+    startInterval();
+}
 
 function showNewQuestion() {
     questions[currentQuestion].style.display = "none";
@@ -86,23 +120,30 @@ function startInterval() {
             timer.innerHTML = timer.innerHTML - 1;
         } else {
             clearInterval(interval)
-            Array.from(buttons).forEach((button) => button.setAttribute("disabled", true))
+            Array.from(buttons).forEach((button) => button.setAttribute("disabled", true));
+            alert("Game over");
         }
     }, 1000)
 }
 
-startInterval();
+function createResetButton(){
+    const resetButton = document.createElement("button");
+    resetButton.innerHTML = "reset game"
+    resetButton.setAttribute("class", "btn-reset");
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", () => {
+        clearInterval(interval);
+        Array.from(buttons).forEach((button) => button.removeAttribute("disabled"));
+        timer.innerHTML = 10;
+        startInterval();
+        questions[currentQuestion].style.display = "none";
+        currentQuestion = 0;
+        questions[currentQuestion].style.display = "block"
+    })
+}
 
-const resetButton = document.createElement("button");
-resetButton.innerHTML = "reset game"
-resetButton.setAttribute("class", "btn-reset");
-document.body.append(resetButton);
-resetButton.addEventListener("click", () => {
-    clearInterval(interval);
-    Array.from(buttons).forEach((button) => button.removeAttribute("disabled"));
-    timer.innerHTML = 10;
-    startInterval();
-    questions[currentQuestion].style.display = "none";
-    currentQuestion = 0;
-    questions[currentQuestion].style.display = "block"
-})
+function createScoreElement(){
+    scoreElement = document.createElement("h4");
+    document.body.append(scoreElement);
+    scoreElement.innerHTML = "Your score";
+}
